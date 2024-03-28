67063e0f17d2b5f673d94dad2992b1dc4480f94e
aissamboudi <aissamboudi@gmail.com>
2024-03-28 14:56:59 +0000

    Update

--- push.c
+++ push.c
@@ -7,6 +7,23 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	(void) stack;
-	(void) line_number;
+	stack_t *node;
+	stack_t *current;
+	char *token = NULL;
+
+	token = strtok(NULL, " \n");
+	printf("{%s}\n", token);
+	current = *head;
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+	if (current)
+		current->prev = node;
+	node->n = n;
+	node->next = *head;
+	node->prev = NULL;
+	*head = node;
 }
